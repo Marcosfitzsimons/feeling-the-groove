99701508ae9fc29e8b5f37d7dab6a0073f6290fc
marcosfitzsimons <marcosfitzsimons@gmail.com>
2024-01-07 23:10:36 -0300

    Update logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import { Icons } from "./icons";
 
 const Logo = () => {
   return (
     <div className="flex flex-col items-center">
-      <Icons.logo className="mx-auto h-6 w-6" />
+      <Link href="/">
+        <Icons.logo className="mx-auto h-6 w-6" />
+      </Link>
       <span className="text-xs font-medium relative bottom-0.5 select-none">
         Feeling the Groove
       </span>
