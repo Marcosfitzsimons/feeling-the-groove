b5523162386bceb993bffe6de68da5443f609936
marcosfitzsimons <marcosfitzsimons@gmail.com>
2024-01-10 11:48:19 -0300

    Add sonner to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { siteConfig } from "@/config/site";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
           <Header />
           <main className="min-h-screen">{children}</main>
           <Footer />
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
