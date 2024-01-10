8eb300569f28d2ccc35527eefc5ef531e8c60b5c
marcosfitzsimons <marcosfitzsimons@gmail.com>
2024-01-10 11:46:48 -0300

    Add post validations

--- validations/rave.ts
+++ validations/rave.ts
@@ -17,4 +17,26 @@ export const ravePatchSchema = z.object({
   genre: z.string().min(1),
   location: z.string().min(1),
   rank: z.number().int(),
+  date: z.date()
+})
+
+
+export const ravePostSchema = z.object({
+  name: z.string().min(1),
+  candy: z.string().optional(),
+  anecdotes: z.string().optional(),
+  quantity: z.number().optional().superRefine((value, ctx) => {
+    if (value !== undefined && value <= 0) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Quantity must be greater than 0",
+      });
+    }
+    return value;
+  }),
+  ayn: z.number().int(),
+  genre: z.string().min(1),
+  location: z.string().min(1),
+  rank: z.number().int(),
+  date: z.date()
 })
