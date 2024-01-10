8aa0f0072796ca19a2903e6caafc0b9751f5f470
marcosfitzsimons <marcosfitzsimons@gmail.com>
2024-01-10 11:43:07 -0300

    Add file to interact with db

---
+++ lib/raves.ts
@@ -0,0 +1,50 @@
+import { Rave } from "@/types";
+import { db } from "./db"
+
+export const getAllRaves = async () => {
+    const data = await db.rave.findMany() 
+
+    const formattedData = data.map((item: Rave) => ({
+        ...item,
+        quantity: item?.quantity?.toNumber(), // Convert 'quantity' Decimal to a number
+        // Convert other Decimal values similarly if needed
+      }));
+    
+      return formattedData; // 
+}
+
+export const getRave = async (id: string) => {
+    const data = await db.rave.findUnique({
+        where: {
+          id: id,
+        },
+    });
+    const formattedData = {
+        ...data,
+        quantity: data?.quantity?.toNumber()
+    }
+    return formattedData
+}
+
+export const getNearestPastRave = async () => {
+    const currentDate = new Date(); 
+    const data = await db.rave.findMany({
+      orderBy: {
+        date: 'desc', // Order by date in descending order
+      },
+      take: 1, // Retrieve only the closest date
+      where: {
+        date: {
+          lt: currentDate, // Get dates less than the current date
+        },
+      },
+    });
+  
+    if (!data || data.length === 0) {
+      return null;
+    }
+  
+    const nearestPastDate = data[0].date
+    
+    return nearestPastDate;
+};
