9b3b9b6f6810e4434eccfa4c27975b8caac16c6a
marcosfitzsimons <marcosfitzsimons@gmail.com>
2024-01-11 17:49:09 -0300

    Add new UI component

--- components/ui/star-rating.tsx
+++ components/ui/star-rating.tsx
@@ -1,4 +1,3 @@
-import { useTheme } from "next-themes";
 import { Icons } from "../icons";
 
 interface StarRatingProps {
@@ -7,22 +6,17 @@ interface StarRatingProps {
 
 export const StarRating = ({ rank }: StarRatingProps) => {
   const stars = [];
-  const { theme } = useTheme();
 
   for (let i = 0; i < rank; i++) {
     stars.push(
       <Icons.star
         key={i}
         className="w-[18px] h-[18px]"
-        fill={theme === "light" ? "#fcd34d" : "#fef08a"}
+        fill="#fef08a"
         strokeWidth={0}
       />
     );
   }
 
-  return (
-    <div className="flex items-center justify-center gap-1 bg-accent p-0.5 px-2 rounded-md">
-      {stars}
-    </div>
-  );
+  return <div className="flex items-center justify-center gap-1">{stars}</div>;
 };
