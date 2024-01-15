9658ac77b76543e89c485634abb33de297d4df3e
marcosfitzsimons <marcosfitzsimons@gmail.com>
2024-01-15 12:47:55 -0300

    Add update rave action

--- app/actions/actions.ts
+++ app/actions/actions.ts
@@ -1,12 +1,17 @@
 "use server"
 
-import { createRave, deleteRave } from "@/lib/raves"
-import { RavePayload } from "@/types"
+import { createRave, deleteRave, updateRave } from "@/lib/raves"
+import { RavePayload, UpdateRavePayload } from "@/types"
 import { revalidatePath } from "next/cache"
 
 export const createRaveAction = async (payload: RavePayload) => {
         await createRave(payload)
         revalidatePath('/raves')
+}
+
+export const updateRaveAction = async (id: string, payload: UpdateRavePayload) => {
+        await updateRave(id, payload)
+        revalidatePath(`/raves/${id}`)
 }
 
 export const deleteRaveAction = async (id: string) => {
