409270c342867bb889340ebd9911d4df818209d7
marcosfitzsimons <marcosfitzsimons@gmail.com>
2024-01-16 12:33:58 -0300

    Add dialog component

--- components/ui/star-rating.tsx
+++ components/ui/star-rating.tsx
@@ -1,11 +1,14 @@
+"use client";
+
 import { useTheme } from "next-themes";
 import { Icons } from "../icons";
 
 interface StarRatingProps {
   rank: number;
+  isLarge?: boolean;
 }
 
-export const StarRating = ({ rank }: StarRatingProps) => {
+export const StarRating = ({ rank, isLarge }: StarRatingProps) => {
   const { theme } = useTheme();
   const stars = [];
 
@@ -13,7 +16,7 @@ export const StarRating = ({ rank }: StarRatingProps) => {
     stars.push(
       <Icons.star
         key={i}
-        className="w-[18px] h-[18px]"
+        className={isLarge ? "w-[22px] h-[22px]" : "w-[18px] h-[18px]"}
         fill={theme === "dark" ? "#fef08a" : "#fcd34d"}
         strokeWidth={0}
       />
