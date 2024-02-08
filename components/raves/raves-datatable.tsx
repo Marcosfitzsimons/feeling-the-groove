ad3332ddc3531441bbb3d592a37f061270d56e2d
marcosfitzsimons <marcosfitzsimons@gmail.com>
2024-02-08 12:41:21 -0300

    Change empty datatable message

--- components/raves/raves-datatable.tsx
+++ components/raves/raves-datatable.tsx
@@ -301,7 +301,7 @@ export function RavesDatatable<TData, TValue>({
                   colSpan={columns.length}
                   className="h-24 text-center"
                 >
-                  No results.
+                  No raves have been found
                 </TableCell>
               </TableRow>
             )}
